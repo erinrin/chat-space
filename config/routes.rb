43101d89dad10c3eb3206e2a70201d100d9d8ffb
erinrin <happyending24.7@gmail.com>
2019-09-13 18:26:20 +0900

    unicorn8

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # devise_for :users
+  devise_for :users
   root "groups#index"
   resources :users, only: [:edit, :update,:index]
   resources :groups, only: [:new, :create, :edit, :update] do
